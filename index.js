8e8efa299309001fa03cab889f51aed02b9bbb4d
samuel nyota karanja <samuel.karanja3@student.moringaschool.com>
2025-06-06 12:27:10 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,9 @@
+function divide(num1 = 2000, num2 = 100){
+    return num1 / num2
+}
+const square = (parameter1 = 2) =>{
+    return parameter1 * parameter1
+}
+const add = (parameter1 = 4, parameter2 = 3)=>{
+    return parameter1 + parameter2
+} 
